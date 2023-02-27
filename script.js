ad411bf494fd878b73fa48b9836cdb859f072200
yasin <123163314+Y4s1n1980@users.noreply.github.com>
2023-02-27 16:56:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const buttons = document.querySelectorAll(".button");
+const body = document.querySelector("body");
+
+console.log(buttons);
+
+buttons.forEach(function(button) {
+  button.addEventListener("click", function(e) {
+    console.log(e.target);
+    if (e.target.id === "red") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "yellow") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "white") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "grey") {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === "orange") {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});
+  
+setInterval(() => {
+    intermittentText.classList.toggle("blink");
+  }, 1000);
